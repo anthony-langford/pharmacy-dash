757c07d71d1295220bb6ac0bbe39e165a6eaabd6
Anthony Langford <anthony.langford@gmail.com>
2018-09-26 16:47:24 -0400

    create patient card component

---
+++ src/components/Dashboard/PatientCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PatientCard = styled.div`
+  display: flex;
+  height: 100px;
+  width: 270px;
+  border: 1px solid black;
+  padding: 16px;
+  align-items: center;
+`;
+
+const PatientName = styled.span`
+  margin: 0px 16px;
+  width: 100%;
+  font-size: 20px;
+`;
+
+export default ({patientName}) => {
+  return (
+    <PatientCard>
+      <img src="https://via.placeholder.com/80x80" alt={'PatientImage'} />
+      <PatientName>{patientName}</PatientName>
+    </PatientCard>
+  );
+};

2cf8a9f0188054d1b05821f6ee173ca64692012f
Anthony Langford <anthony.langford@gmail.com>
2018-09-29 14:47:50 -0400

    change to use styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Dashboard from './Dashboard';
+
+const App = styled.div`
+  text-align: center;
+`;
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <App>
         <Dashboard />
-      </div>
+      </App>
     );
   };
 };
